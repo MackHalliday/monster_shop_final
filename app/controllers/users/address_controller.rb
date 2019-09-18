cc38dea05db24a7ca35d679781da3bac25b50500
MackHalliday <mackenzie.halliday@gmail.com>
2019-09-17 21:43:03 -0600

    Update folder name

---
+++ app/controllers/users/address_controller.rb
@@ -0,0 +1,16 @@
+class Users::AddressController < ApplicationController
+  def edit
+    @address = current_user.addresses.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to profile_path
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:address_type, :name, :address, :city, :state, :zipcode)
+  end
+end
